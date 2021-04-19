f990d8c4fa9aad1c7d4b834b72215c14f0ac31e8
Israel Molestina <molestinaisrael@gmail.com>
2021-04-19 18:31:40 -0400

    added routes for the html static pages

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,17 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const Workout = require('../models/workout');
 
+router.post('/api/workouts', ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,13 +14,14 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 // routes
-// app.use(require('./routes/api.js'));
+app.use(require('./routes/api'));
+app.use(require('./routes/html'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
