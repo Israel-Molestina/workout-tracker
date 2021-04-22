ebe1faef3b476a3510345033dfebf3fd81ab5953
Israel Molestina <molestinaisrael@gmail.com>
2021-04-22 19:41:10 -0400

    deploying

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -16,6 +15,8 @@ app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
